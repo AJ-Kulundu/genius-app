458556c140ee060557983df97b4186a3ceaa4def
AJ-Kulundu <jameskulundu@gmail.com>
2022-04-09 11:43:40 +0300

    setting up redux toolkit in the web app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { DefaultButton } from "@fluentui/react";
+import { useSelector,useDispatch } from 'react-redux';
+import { increment,decrement } from '../features/counter/counterSlice'
 
 export default function Home() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch();
   return (
     <div className={styles.container}>
       <Head>
@@ -16,10 +21,12 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <br />
+                      <span>{count}</span>
+        <div className={styles.grid}>
+          <DefaultButton text="increment" onClick={() => dispatch(increment())} />
+          <DefaultButton text="decrement" onClick={() => dispatch(decrement())} />
+        </div>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -58,12 +65,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
